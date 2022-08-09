72b1378497250a3ffbd2636fd27b500971a211bd
marianadick <dickmariana1@gmail.com>
2022-08-09 16:01:19 -0300

    connect form to api

--- frontend/src/components/MainForm.js
+++ frontend/src/components/MainForm.js
@@ -4,17 +4,47 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 class MainForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) { this.setState({ value: event.target.value }); }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+
+        const dados = { k: this.state.value, result: "0" };
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', "Access-Control-Allow-Origin":"*" },
+            body: JSON.stringify(dados)
+        };
+
+        const response = await fetch('http://localhost:8080/input', requestOptions);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        this.setState({ value: data.result });
+
+    }
+
     render() {
         return (
-            <Form className="m-10">
+            <Form className="m-10" onSubmit={this.handleSubmit}>
                 <Form.Group className="mb-3" controlId="numberInput">
                     <Form.Label>Número</Form.Label>
-                    <Form.Control type="number" step="1" placeholder="Exemplo: 5" />
+                    <Form.Control onChange={this.handleChange} name="numInput" value={this.state.value} type="number" step="1" placeholder="Exemplo: 5" />
                     <Form.Text className="text-muted">
                         Digite um número inteiro.
                     </Form.Text>
                 </Form.Group>
-                <Button variant="primary" type="submit" style={{"width": "100%"}}>
+                <Button variant="primary" type="submit" style={{ "width": "100%" }}>
                     Calcular
                 </Button>
             </Form>
